9d25d581ffda661c213bef184682d30df734810d
e277 <ezramuir12@gmail.com>
2023-11-15 05:10:00 -0500

    Exercise 4 - Display Additional Countries Information in a Table

--- world.php
+++ world.php
@@ -1,4 +1,5 @@
 <?php
+
 $host = 'localhost';
 $username = 'lab5_user';
 $password = 'password123';
@@ -10,13 +11,29 @@ $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $p
 $country = $_GET['country'];
 $country = filter_var($country, FILTER_SANITIZE_SPECIAL_CHARS);
 
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+try {
+    $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
+    echo "<table>";
+    echo "<tr>";
+    echo "<th>Name</th>";
+    echo "<th>Continent</th>";
+    echo "<th>Independence</th>";
+    echo "<th>Head of State</th>";
+    echo "</tr>";
+
+    foreach ($results as $row) {
+        echo "<tr>";
+        echo "<td>" . $row['name'] . "</td>";
+        echo "<td>" . $row['continent'] . "</td>";
+        echo "<td>" . $row['independence_year'] . "</td>";
+        echo "<td>" . $row['head_of_state'] . "</td>";
+        echo "</tr>";
+    }
+    echo "</table>";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
