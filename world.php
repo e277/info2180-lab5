76fcc8f3f5e6fb7db3c5e48a1af34f769311d30d
e277 <ezramuir12@gmail.com>
2023-11-14 21:39:33 -0500

    Exercise 2 - MySQL in PHP

--- world.php
+++ world.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $username = 'lab5_user';
-$password = '';
+$password = 'password123';
 $dbname = 'world';
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
@@ -10,6 +10,8 @@ $stmt = $conn->query("SELECT * FROM countries");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+
 <ul>
 <?php foreach ($results as $row): ?>
   <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
